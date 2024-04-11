474f04ebf2476c8ac8a38b165c3a649d1ca52596
Property404 <git@dagans.dev>
2024-04-11 17:05:22 -0500

    Better clamping

--- Clamped.hpp
+++ Clamped.hpp
@@ -18,7 +18,102 @@ class Clamped {
         assert(val <= max);
     }
 
+    T inner() const {
+        return this->_val;
+    }
+
+    bool operator ==(T val) const {
+        return _val == val;
+    }
+
+    bool operator >=(T val) const {
+        return _val >= val;
+    }
+
+    bool operator >(T val) const {
+        return _val > val;
+    }
+
+    bool operator <=(T val) const {
+        return _val <= val;
+    }
+
+    bool operator <(T val) const {
+        return _val < val;
+    }
+
+    friend std::ostream& operator<< (std::ostream& stream, const Clamped<T>& that) {
+        return (stream << that.inner());
+    }
+
+    Clamped<T> operator -(T val) const {
+        if (_min >= 0 && val > _val) {
+            return Clamped(0, _min, _max);
+        }
+        return Clamped(std::clamp(_val - val, _min, _max), _min, _max);
+    }
+
     Clamped<T> operator +(T val) const {
-        return this->_val + val;
+        return Clamped(std::clamp(_val + val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator *(T val) const {
+        return Clamped(std::clamp(_val * val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator /(T val) const {
+        return Clamped(std::clamp(_val / val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator %(T val) const {
+        return Clamped(std::clamp(_val % val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator ^(T val) const {
+        return Clamped(std::clamp(_val ^ val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator &(T val) const {
+        return Clamped(std::clamp(_val & val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator |(T val) const {
+        return Clamped(std::clamp(_val | val, _min, _max), _min, _max);
+    }
+
+    Clamped<T> operator =(T val){
+        return (*this = Clamped(std::clamp(val,_min,_max), _min, _max));
+    }
+
+    Clamped<T> operator -=(T val){
+        return (*this = *this - val);
+    }
+
+    Clamped<T> operator +=(T val){
+        return (*this = *this + val);
+    }
+
+    Clamped<T> operator ++(int){
+        const auto old = *this;
+        *this += 1;
+        return old;
+    }
+
+    Clamped<T> operator --(int){
+        const auto old = *this;
+        *this -= 1;
+        return old;
+    }
+
+    Clamped<T> operator *=(T val){
+        return (*this = *this * val);
+    }
+
+    Clamped<T> operator /=(T val){
+        return (*this = *this / val);
+    }
+
+    Clamped<T> operator %=(T val){
+        return (*this = *this % val);
     }
 };
