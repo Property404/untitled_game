4e39ab50a5eb0eb8a4762007fc74ed1f36f3579d
Property404 <git@dagans.dev>
2024-04-15 14:16:43 -0500

    Focus canvas automatically

--- main.cpp
+++ main.cpp
@@ -67,6 +67,13 @@ void main_loop(void* ptr) {
 
 int main() {
     static auto game_data = Game(WIDTH, HEIGHT);
+
+    EM_ASM_(
+        {
+            Module.canvas = document.querySelector("#canvas");
+            Module.canvas.focus();
+        });
+
     emscripten_set_keydown_callback("#canvas", &game_data, 1, keydown);
     emscripten_set_keyup_callback("#canvas", &game_data, 1, keyup);
     emscripten_set_main_loop_arg(main_loop, &game_data, FRAME_RATE, 1);
